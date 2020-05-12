4a96c8525f313a3173910bc7d26a27c46e54b10b
Mayya-S <65227777+Mayya-S@users.noreply.github.com>
2020-05-12 16:46:49 +0400

    deyisdim
    deyisdim

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 ?>
 <!-- Tell the browser to be responsive to screen width -->
 <!-- Tell the browser to be responsive to screen width -->
+<!-- Tell the browser to be responsive to screen width -->
 <!DOCTYPE html>
 <html>
 <head>
